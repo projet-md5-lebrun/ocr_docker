8793e182568318910ad44e3c84d9a6a37773fd09
Camille Lebrun <116400782+camlebrun@users.noreply.github.com>
2024-01-16 16:18:00 +0100

    feat/ front and clean code for OCR

--- app/app.py
+++ app/app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request
 import cv2
 import numpy as np
 import easyocr
-
+import os
 app = Flask(__name__)
 
-reader = easyocr.Reader(['en'])  # Cette ligne doit être exécutée une seule fois pour charger le modèle en mémoire
+reader = easyocr.Reader(['en'])  
 
 def easyocr_text(img):
     results = reader.readtext(img)
@@ -13,19 +13,23 @@ def easyocr_text(img):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    results = None
+    texts = None
+
     if request.method == 'POST':
         file = request.files['image']
-        
+
         if file:
             image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
             results = easyocr_text(image)
-            
+
             # Extract the texts from results
             texts = [result[1] for result in results]
-            
-            return render_template('index.html', results=results, image_path="your_image.jpg", texts=texts)
+            # concat all texts
+            texts = ' '.join(texts)
+            print(texts)
 
-    return render_template('index.html', results=None, image_path=None, texts=None)
+    return render_template('index.html', results=results, texts=texts)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=8000, debug=True)
