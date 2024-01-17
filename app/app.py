784099211693c0cc9fd3f3c8fd45c7332ab7183e
Camille Lebrun <116400782+camlebrun@users.noreply.github.com>
2024-01-17 10:03:37 +0100

    chlore

--- app/app.py
+++ app/app.py
@@ -3,30 +3,33 @@ import cv2
 import numpy as np
 import easyocr
 import os
-
+import text_to_speech
 app = Flask(__name__)
 
-reader = easyocr.Reader(['en'])  # Cette ligne doit être exécutée une seule fois pour charger le modèle en mémoire
+reader = easyocr.Reader(['fr'])  
 
 def easyocr_text(img):
-     results = reader.readtext(img)
-     return results
+    results = reader.readtext(img)
+    return results
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-     if request.method == 'POST':
-         file = request.files['image']
+    results = None
+    image_path = None
+    texts = None
 
-         if file:
-             image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
-             results = easyocr_text(image)
+    if request.method == 'POST':
+        file = request.files['image']
 
-             # Extract the texts from results
-             texts = [result[1] for result in results]
+        if file:
+            image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
+            results = easyocr_text(image)
 
-             return render_template('index.html', results=results, image_path="your_image.jpg", texts=texts)
+            # Extract the texts from results
+            texts = [result[1] for result in results]
+            image_path = "your_image.jpg"
 
-     return render_template('index.html', results=None, image_path=None, texts=None)
+    return render_template('index.html', results=results, image_path=image_path, texts=texts)
 
 if __name__ == '__main__':
-     app.run(debug=True, port=5001 , host='0.0.0.0' )
+    app.run(host='0.0.0.0', port=8000, debug=True)
